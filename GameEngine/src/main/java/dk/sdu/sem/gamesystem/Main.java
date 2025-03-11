7a79ae870f020fb474fbad4253422f1a59279a7c
David H <davidhulstroem@gmail.com>
2025-03-11 14:08:50 +0100

    Javafx Scene setup

--- GameEngine/src/main/java/dk/sdu/sem/gamesystem/Main.java
+++ GameEngine/src/main/java/dk/sdu/sem/gamesystem/Main.java
@@ -1,9 +1,13 @@
 package dk.sdu.sem.gamesystem;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private final Pane gameWindow = new Pane();
+
     public static void main(String[] args) {
         launch(Main.class);
     }
@@ -11,6 +15,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("Rachend");
+
+		Scene scene = new Scene(gameWindow);
+
+		stage.setScene(scene);
 		stage.show();
 	}
 }
