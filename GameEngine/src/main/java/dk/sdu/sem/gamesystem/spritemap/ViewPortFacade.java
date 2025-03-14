1215e0a4c0687ce6a74295b45c61a01869d8086d
zenrough <12938899-Zenrough@users.noreply.gitlab.com>
2025-03-14 17:24:50 +0100

    Added Factory and Facade for viewport

---
+++ GameEngine/src/main/java/dk/sdu/sem/gamesystem/spritemap/ViewPortFacade.java
@@ -0,0 +1,29 @@
+package dk.sdu.sem.gamesystem.spritemap;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+// this class is for providing ViewPorts of the
+// spritemap to make code more readable
+// or the viewports can be made at runtime via ViewPortFactory
+public class ViewPortFacade {
+Map<String,ViewPort> map;
+
+	public static void main(String[] args) {
+		ConcurrentHashMap<String,ViewPort>	viewPortMap = new ConcurrentHashMap<>();
+		ViewPortFactory viewPortFactory = new ViewPortFactory();
+		// below will not do anything as no spritemap is loaded.
+		ViewPort viewPort = viewPortFactory.createViewPort(0,0,0,400,600);
+		viewPortMap.put("playerOne",viewPort);
+
+
+		// this could be automated by using the filename as the key in the
+		// filepath
+		// hard write values for Image path and add
+		// arbitrary names for the sprite here
+
+
+	}
+}
+
+
