58299f1e5ba8dfa3a65a2ebc253be56ddfc7f914
MV <mving23@student.sdu.dk>
2025-03-20 17:44:44 +0100

    Fixed re-added imports to Physics module-info

--- Physics/src/main/java/module-info.java
+++ Physics/src/main/java/module-info.java
@@ -1,11 +1,19 @@
+import dk.sdu.sem.commonsystem.Node;
+import dk.sdu.sem.physicssystem.PhysicsNode;
+import dk.sdu.sem.commonsystem.INodeProvider;
+import dk.sdu.sem.physicssystem.PhysicsNodeProvider;
+import dk.sdu.sem.gamesystem.services.IFixedUpdate;
+import dk.sdu.sem.physicssystem.PhysicsSystem;
+import dk.sdu.sem.gamesystem.services.IUpdate;
+
 module Physics {
 	requires Common;
 	requires GameEngine;
 
-	provides dk.sdu.sem.commonsystem.Node with dk.sdu.sem.physicssystem.PhysicsNode;
-	provides dk.sdu.sem.commonsystem.INodeProvider with dk.sdu.sem.physicssystem.PhysicsNodeProvider;
-	provides dk.sdu.sem.gamesystem.services.IFixedUpdate with dk.sdu.sem.physicssystem.PhysicsSystem;
-	provides dk.sdu.sem.gamesystem.services.IUpdate with dk.sdu.sem.physicssystem.PhysicsSystem;
+	provides Node with PhysicsNode;
+	provides INodeProvider with PhysicsNodeProvider;
+	provides IFixedUpdate with PhysicsSystem;
+	provides IUpdate with PhysicsSystem;
 
 	exports dk.sdu.sem.physicssystem;
 }
