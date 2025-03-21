d2a05b67b0aae241808fc8b790f4d29a06d9d537
Phillip713 <Phillip713@users.noreply.github.com>
2025-03-21 10:27:15 +0100

    Added EnemyComponent with speed attribute

---
+++ CommonEnemy/src/main/java/dk/sdu/sem/EnemyComponent.java
@@ -0,0 +1,20 @@
+package dk.sdu.sem;
+
+import dk.sdu.sem.commonsystem.IComponent;
+
+public class EnemyComponent implements IComponent {
+
+	private float moveSpeed = 1.0f;
+
+	public EnemyComponent() {
+		this(1.0f);
+	}
+
+	public EnemyComponent(float moveSpeed) {
+		this.moveSpeed = moveSpeed;
+	}
+
+	public float getMoveSpeed() {
+		return moveSpeed;
+	}
+}
