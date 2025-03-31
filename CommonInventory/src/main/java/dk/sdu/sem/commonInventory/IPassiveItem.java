9647785c370638de4033cf2f6da25395011ba81e
Ryvy <mikkelbjoernmartinsen@gmail.com>
2025-03-31 19:53:48 +0200

    Pickup function for passive items

--- CommonInventory/src/main/java/dk/sdu/sem/commonInventory/IPassiveItem.java
+++ CommonInventory/src/main/java/dk/sdu/sem/commonInventory/IPassiveItem.java
@@ -1,4 +1,6 @@
 package dk.sdu.sem.commonInventory;
 
 public interface IPassiveItem extends IItem {
+
+	public void onPickUp();
 }
--- Item/src/main/java/dk/sdu/sem/itemsystem/PassiveItem.java
+++ Item/src/main/java/dk/sdu/sem/itemsystem/PassiveItem.java
@@ -4,4 +4,8 @@ import dk.sdu.sem.commonInventory.IItem;
 import dk.sdu.sem.commonInventory.IPassiveItem;
 
 public class PassiveItem implements IPassiveItem {
+
+	public void onPickUp() {
+
+	}
 }
